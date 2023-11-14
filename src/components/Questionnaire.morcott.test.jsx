7eab60dcb63dbc69cc58394746e8df8e0e7b9390
cmorcott <coelmorcott2024@u.northwestern.edu>
2023-11-14 13:08:32 -0600

    One working one fail

---
+++ src/components/Questionnaire.morcott.test.jsx
@@ -0,0 +1,58 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import Questionnaire from './Questionnaire';
+
+vi.mock('../utilities/firebase', async () => {
+    const actual = await vi.importActual('../utilities/firebase');
+    return {
+        ...actual,
+        useAuthState: vi.fn(() => [{}, null]),
+    };
+});
+
+const mockNavigate = vi.fn();
+vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom');
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate,
+    };
+});
+
+describe('questionnaire submit button', () => {
+    beforeEach(() => {
+        mockNavigate.mockClear();
+    });
+
+    it('should navigate to recommendations page on submit despite incomplete questionnaire answers', async () => {
+        const mockSetResults = vi.fn();
+        render(
+            <MemoryRouter initialEntries={['/questionnaire']}>
+                <Routes>
+                    <Route path="/questionnaire" element={<Questionnaire setResults={mockSetResults} />} />
+                    <Route path="/recommendations" element={<div>Recommendations Page</div>} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        //Selecting partial answers here
+        const firstQuestionAnswer = screen.getByText(/Very Active:/i);
+        userEvent.click(firstQuestionAnswer);
+
+        const secondQuestionAnswer = screen.getByText(/Very Independent:/i);
+        userEvent.click(secondQuestionAnswer);
+
+        const thirdQuestionAnswer = screen.getByText(/Several Hours:/i);
+        userEvent.click(thirdQuestionAnswer);
+
+        // Click the submit button
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(mockNavigate).toHaveBeenCalledWith('/recommendations');
+        });
+    });
+});
--- src/components/RecommendationList.morcott.test.jsx
+++ src/components/RecommendationList.morcott.test.jsx
@@ -1,38 +1,49 @@
-import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import RecommendationList from './RecommendationList';
+import Questionnaire from './Questionnaire'; // Ensure this is imported if needed
 
-// Mocking Firebase utilities if needed
 vi.mock('../utilities/firebase', async () => {
     const actual = await vi.importActual('../utilities/firebase');
     return {
         ...actual,
-        useAuthState: vi.fn(() => [{}, null]), // Mocking useAuthState
+        useAuthState: vi.fn(() => [{}, null]),
+    };
+});
+
+const mockNavigate = vi.fn();
+vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom');
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate,
     };
 });
 
 describe('back to questionnaire button', () => {
-    // Mock results array with the correct structure
     const res = [
         { pet: { Image: 'image1.jpg', Name: 'Pet 1', Breed: 'Breed 1', Activity: 4, Age: 5, Temperment: 3} },
         { pet: { Image: 'image1.jpg', Name: 'Pet 1', Breed: 'Breed 1', Activity: 4, Age: 5, Temperment: 3} }
     ];
 
     it('should navigate back to questionnaire', async () => {
+        const mockSetResults = vi.fn();
         render(
             <MemoryRouter initialEntries={['/recommendations']}>
                 <Routes>
                     <Route path="/recommendations" element={<RecommendationList results={res} />} />
-                    <Route path="/questionnaire" element={<div>Questionnaire Page</div>} />
+                    <Route path="/questionnaire" element={<Questionnaire setResults={mockSetResults} />} />
                 </Routes>
             </MemoryRouter>
         );
 
-        const backButton = screen.getByText("Back to Questionnaire");
+        const backButton = screen.getByRole('link', { name: /back to questionnaire/i });
         userEvent.click(backButton);
 
-        // Check for navigation (mock useNavigate and verify it's called with the correct path)
+        await waitFor(() => {
+            expect(mockNavigate).toHaveBeenCalledWith('/questionnaire');
+        });
     });
 });
